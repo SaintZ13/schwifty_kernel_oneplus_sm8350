SaintZ13/schwifty_kernel_oneplus_sm8150

schwifty: build script changes

build.sh
@@ -1,31 +1,18 @@
 #!/bin/bash
 
-# HOME path
-export HOME=/home/saintz
+# Kernel version configuration
+KNAME="SchwiftyKernel-OP7PRO"
+MIN_HEAD=$(git rev-parse HEAD)
+VERSION="$(cat version)-$(date +%m.%d.%y)-$(echo ${MIN_HEAD:0:8})"
+ZIPNAME="${KNAME}-$(cat version)-$(echo ${MIN_HEAD:0:8})"
 
-echo
-echo "Clean Build Directory"
-echo 
+export LOCALVERSION="-${KNAME}-$(echo "${VERSION}")"
 
-make clean && make mrproper
+# Start Timer
+START=$(date +"%s")
+./source.sh || exit 1
 
-# Compiler environment
-mkdir -p out
-export CLANG_PATH=$HOME/proton-clang/install/bin
-export PATH="$CLANG_PATH:$PATH"
-export CROSS_COMPILE=aarch64-linux-gnu-
-export CROSS_COMPILE_ARM32=arm-linux-gnueabi-
-export KBUILD_BUILD_USER=SaintZ13
-export KBUILD_BUILD_HOST=1337
-
-echo
-echo "Setting defconfig"
-echo
-
-make CC=clang LD=ld.lld AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip O=out schwifty_defconfig
-
-echo
-echo "Compiling kernel"
-echo
-
-make CC=clang LD=ld.lld AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip O=out -j$(nproc --all) || exit 1
+# End Timer
+END=$(date +"%s")
+DIFF=$((END - START))
+echo -e "Kernel compiled successfully in $((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s)"

SaintZ13/schwifty_kernel_oneplus_sm8350

build.sh
@@ -1,0 +1,18 @@
+#!/bin/bash
+
+# Kernel version configuration
+KNAME="SchwiftyKernel-OP9PRO"
+MIN_HEAD=$(git rev-parse HEAD)
+VERSION="$(cat version)-$(date +%m.%d.%y)-$(echo ${MIN_HEAD:0:8})"
+ZIPNAME="${KNAME}-$(cat version)-$(echo ${MIN_HEAD:0:8})"
+
+export LOCALVERSION="-${KNAME}-$(echo "${VERSION}")"
+
+# Start Timer
+START=$(date +"%s")
+./source.sh || exit 1
+
+# End Timer
+END=$(date +"%s")
+DIFF=$((END - START))
+echo -e "Kernel compiled successfully in $((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s)"